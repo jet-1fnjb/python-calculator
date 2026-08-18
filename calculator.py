8ecc99386fbeadab49832c9981f0ac7a1b4d103d
jet-1fnjb <techdevil@kali>
2026-08-18 14:59:40 +0000

    Create Python Calculator

---
+++ calculator.py
@@ -0,0 +1,53 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero."
+    return a / b
+
+while True:
+    print("\n===== PYTHON CALCULATOR =====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Choose an option (1-5): ")
+
+    if choice == "5":
+        print("Goodbye!")
+        break
+    if choice not in ["1", "2", "3", "4"]:
+        print("Invalid choice. Please try again.")
+        continue
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Please enter valid number.")
+        continue
+    if choice == "1":
+        result = add(num1, num2)
+        result(f"Result: {result}")
+
+    elif choice == "2":
+        result = subtract(num1, num2)
+        print(f"Result: {result}")
+
+    elif choice == "3":
+        result = multiply(num1, num2)
+        print(f"Result: {result}")
+
+    elif choice == "4":
+        result = divide(num1, num2)
+        print(f"Result: {result}")
+        
